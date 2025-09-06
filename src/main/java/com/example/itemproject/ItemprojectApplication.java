e83635db8aa12bd2fab887eea3372345f12ed85e
kumuthiniak <kumuthini.program@gmail.com>
2025-09-06 17:07:44 +0530

    initial commit

---
+++ src/main/java/com/example/itemproject/ItemprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itemproject/ItemprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
